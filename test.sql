5f6b76d5882d370c776aa337e9b7f21bd085f623
Alexandre GESP <alexandre.gesp@gmail.com>
2025-02-18 07:16:14 +0100

    xxx

--- test.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (nom) VALUES ('Richard DASSAULT');
-INSERT INTO users (nom) VALUES ('Mélusine Enfayite');
-INSERT INTO users (nom) VALUES ('Harry Cover');
-INSERT INTO users (nom) VALUES ('Agripine De Cheuvalle');
-INSERT INTO users (nom) VALUES ('Albert AIBASK');
-INSERT INTO users (nom) VALUES ('Marie GAULE');
